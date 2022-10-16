05efe5bd348bc5f4b4f68b71c0e7916ae0402d5c
Merlin0110 <115934323+Merlin0110@users.noreply.github.com>
2022-10-16 15:39:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 x = 10000      #x ist die aktuelle Anzahl der noch übrigen Kokosnüsse 
 b = 10000      #b ist die anzahl der Kokosnüsse, welche erst ganz am ende vom Durchgang geändert wird und zur Berechnung der bekommenen Anzahl an Kokosnüssen benötigt wird 
 Nummer = 2022    #hier kann die Zahl des Piraten eingetragen werden, von welcher man die Anzahl wissen möchte
-for a in range(1, Nummer):   #hier wird für jeden piraten einzeln ein Durchgang gemacht
+for a in range(1, Nummer + 1):   #hier wird für jeden piraten einzeln ein Durchgang gemacht
   y = 2023 - a    #Hier wird berechnet, wie viele Piraten übrig sind
   x = x - round(x/y, 0)    #Hier wird berechnet, wie viele Kokosnüsse der Pirat bekommt und diese Anzahl von den gesamten abgezogen
   print(f"Person {a} bekommt {b-x}")  #hier wird ausgegeben, wie viele Kokosnüsse die jeweilige Person bekommt
